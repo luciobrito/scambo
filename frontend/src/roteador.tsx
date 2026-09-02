e25d52aec99efd295f72154ce005312e9ff085d7
PedroAMoraes <pealcantara@hotmail.com>
2026-09-02 15:33:25 -0300

    Criação página cadastro

--- frontend/src/roteador.tsx
+++ frontend/src/roteador.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router"
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import Cadastro from "./pages/Cadastro/Cadastro";
 
 export const roteador = createBrowserRouter([
     {
@@ -10,5 +11,9 @@ export const roteador = createBrowserRouter([
     {
         path:"/login",
         element: <Login/>
+    },
+    {
+        path:"/cadastro",
+        element: <Cadastro/>
     }
 ]);
