66fc6b416660f277339efa448129ed1cf67579cd
Lucio <luciobp869@gmail.com>
2026-09-05 21:57:45 -0300

    Revert "Form Cadastro como componente"
    Separação do formulário desnecessária e campo children inutilizado

--- frontend/src/pages/Cadastro/Cadastro.tsx
+++ frontend/src/pages/Cadastro/Cadastro.tsx
@@ -1,12 +1,29 @@
 import Botao from "../../components/Botao/Botao";
 import styles from "./Cadastro.module.css";
-import CadastroForm from "./forms/CadastroForm";
 export default function Cadastro(){
     return <>
     <h1>Scambo</h1>
     <h2>Cadastro</h2>
     <div className = {styles.divCustom}>
-        <CadastroForm children={undefined}></CadastroForm>
+        <form className={styles.formCustom}>
+            <label htmlFor="inputNome">Nome:</label>
+            <input id="inputNome" type="text" />
+
+            <label htmlFor="inputEmail">Email:</label>
+            <input id="inputEmail" type="email" />
+
+            <label htmlFor="inputTelefone">Telefone:</label>
+            <input id="inputTelefone" type="text" />
+
+            <label htmlFor="inputSenha">Senha:</label>
+            <input id="inputSenha" type="password" />
+
+            <label htmlFor="inputConfirmarSenha">Confirmar Senha:</label>
+            <input id="inputConfirmarSenha" type="password" />
+            <Botao carregando={false}>Meu botão</Botao>
+        </form>
+                    
+
     </div>
     </>
 }
