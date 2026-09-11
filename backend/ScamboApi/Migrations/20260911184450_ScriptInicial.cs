8b3944b6446e5869b861c6b65a7c25272d1dccac
Lucio <luciobp869@gmail.com>
2026-09-11 15:57:02 -0300

    dotnet ef e migration inicial

---
+++ backend/ScamboApi/Migrations/20260911184450_ScriptInicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScamboApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScriptInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+            //Criar tabelas aqui ↓
+            migrationBuilder.Sql(@"
+                
+            
+            ");
+            migrationBuilder.Sql(@"
+            
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //Para reverter as alterações
+            //migrationBuilder.Sql("DROP TABLE exemplo");
+        }
+    }
+}
